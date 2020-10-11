e89a0076841afb16971d7391e847a5775eda0710
arbonik <zmear@mail.ru>
2020-10-11 22:39:33 +0700

    delete legasy

--- main.py
+++ main.py
@@ -1,13 +1,3 @@
-from Pest import Pest
-from Resource import Resource
-from Stock import Stock
 from StockConsoleView import StockConsoleView
 
-s = Stock()
-s.products = [Resource(),Resource()]
-
-
-sc = StockConsoleView()
-sc.consoleController()
-
-# p.destroyResource()
+StockConsoleView().consoleController()
